0b75c8a8f6a08765c945a7aa145928a5a34842ac
David Rollinson <david@svr0.rollinson.org>
2022-02-28 01:39:07 +0000

    Using Open CV install scripts

--- 06.sh
+++ 06.sh
@@ -5,8 +5,10 @@ source ~/vRMS/bin/activate
 sudo apt-get install -y python3-opencv
 pip install --no-binary opencv-python opencv-python
 
-cd ~/source/RMS
-python setup.py install
+cp install.sh ~
+cp opencv4_install.sh ~
+cd ~
+install.sh
 
 cd ~/source/RMS/Scripts
 ./GenerateDesktopLinks.sh
