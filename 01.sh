f8c888f29e0215e56316b6f0176cc53403b5dbfc
David Rollinson <david@svr0.rollinson.org>
2022-02-28 09:22:46 +0000

    Test again

--- 01.sh
+++ 01.sh
@@ -4,6 +4,7 @@ echo "Step 1 - Preliminaries - enable passwordless shutdown"
 sudo apt-get update
 sudo apt-get upgrade -y
 sudo apt-get install curl git -y
+sudo apt-get remove -y --purge libreoffice
 sudo timedatectl set-timezone UTC
 sudo cp pi /etc/sudoers.d/
 
--- setres.sh
+++ setres.sh
@@ -2,11 +2,12 @@
 cvt 1680 1050 60
 activemonitor=$(xrandr --listactivemonitors | rev | cut -f1 --d ' ' | rev | tail -n1)
 echo "$activemonitor"
+# Modeline "1280x960_60.00"  101.25  1280 1360 1488 1696  960 963 967 996 -hsync +vsync
 # 1680x1050 59.95 Hz (CVT 1.76MA) hsync: 65.29 kHz; pclk: 146.25 MHz
 # Modeline "1680x1050_60.00"  146.25  1680 1784 1960 2240  1050 1053 1059 1089 -hsync +vsync
-xrandr --newmode "1680x1050_60.00"  146.25  1680 1784 1960 2240  1050 1053 1059 1089 -hsync +vsync
-xrandr --addmode $activemonitor "1680x1050_60.00"
-xrandr --output $activemonitor --mode 1680x1050_60.00
+xrandr --newmode "1280x960_60.00"  101.25  1280 1360 1488 1696 960 963 967 996 -hsync +vsync
+xrandr --addmode $activemonitor "1280x960_60.00"
+xrandr --output $activemonitor --mode 1280x960_60.00
 sleep 10
 killall conky
 conky &
