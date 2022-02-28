ba47f212d17801362f7702c61c5908d33ceeeb8e
David Rollinson <david@svr0.rollinson.org>
2022-02-28 06:33:54 +0000

    Ready to test

--- 06.sh
+++ 06.sh
@@ -2,8 +2,6 @@
 echo "Step 6 - Install OpenCV"
 
 source ~/vRMS/bin/activate
-#sudo apt-get install -y python3-opencv
-#pip install --no-binary opencv-python opencv-python
 
 cp install.sh ~
 cp opencv4_install.sh ~
